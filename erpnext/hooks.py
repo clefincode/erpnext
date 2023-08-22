1b5f4473c52ae112e8fd295e39dc013b647a2cb4
osamaerek <osama.ereksosi@gmail.com>
2023-08-22 15:13:29 +0200

    add service to repost all cost centers

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -376,7 +376,8 @@ scheduler_events = {
 	],
 	"monthly_long": [
 		"erpnext.accounts.deferred_revenue.process_deferred_accounting",
-		"erpnext.loan_management.doctype.process_loan_interest_accrual.process_loan_interest_accrual.process_loan_interest_accrual_for_demand_loans"
+		"erpnext.loan_management.doctype.process_loan_interest_accrual.process_loan_interest_accrual.process_loan_interest_accrual_for_demand_loans",
+        "erpnext.accounts.doctype.repost_deferred_revenue.repost_deferred_revenue.repost_all_cost_centers"
 	]
 }
 
